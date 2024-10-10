d4d58d85ff0d8a91ff816bd863838dbe43d4486e
PKoev99 <saov3v3v3@gmail.com>
2024-10-10 15:58:04 +0300

    Adding Data constants and Error messages

---
+++ VenomGames.Infrastructure/Constants/DataConstants.cs
@@ -0,0 +1,14 @@
+﻿namespace VenomGames.Infrastructure.Constants
+{
+    public static class DataConstants
+    {
+        public const int GameTitleMaxLength = 100;
+        public const int GameDescriptionMaxLength = 500;
+        public const int CategoryNameMaxLength = 100;
+        public const int ReviewContentMaxLength = 1000;
+        public const int ReviewMinimumRating = 1;
+        public const int ReviewMaximumRating = 5;
+        public const string GameMinimumPrice = "0.01";
+        public const string GameMaximumPrice = "1000";
+    }
+}
---
+++ VenomGames.Infrastructure/Constants/ErrorMessages.cs
@@ -0,0 +1,11 @@
+﻿namespace VenomGames.Infrastructure.Constants
+{
+    public static class ErrorMessages
+    {
+        public const string GameTitleLengthError = "The title must not exceed 100 characters.";
+        public const string GameDescriptionLengthError = "The description must not exceed 500 characters.";
+        public const string CategoryNameLengthError = "The category name must not exceed 100 characters.";
+        public const string ReviewContentLengthError = "The content must not exceed 1000 characters.";
+        public const string GamePriceError = "The price must be between 0.01 and 1000.";
+    }
+}
