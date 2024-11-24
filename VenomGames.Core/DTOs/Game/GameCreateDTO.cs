291aee332d0562cc6e49131ba3e07e382b4eb0cf
PKoev99 <saov3v3v3@gmail.com>
2024-11-24 21:02:38 +0200

    Creating DTOs for Game

---
+++ VenomGames.Core/DTOs/Game/GameCreateDTO.cs
@@ -0,0 +1,11 @@
+﻿namespace VenomGames.Core.DTOs.Game
+{
+    public class GameCreateDTO
+    {
+        public int GameId { get; set; }
+        public string Title { get; set; }
+        public string Description { get; set; }
+        public decimal Price { get; set; }
+        public int CategoryId { get; set; }
+    }
+}
---
+++ VenomGames.Core/DTOs/Game/GameUpdateDTO.cs
@@ -0,0 +1,10 @@
+﻿namespace VenomGames.Core.DTOs.Game
+{
+    public class GameUpdateDTO
+    {
+        public string Title { get; set; }
+        public string Description { get; set; }
+        public decimal Price { get; set; }
+        public int CategoryId { get; set; }
+    }
+}
