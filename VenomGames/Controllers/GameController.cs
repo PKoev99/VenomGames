06e98e8e0f8b0acb208d9b1661d9d8d17f7cdb3f
PKoev99 <saov3v3v3@gmail.com>
2024-11-24 21:02:55 +0200

    Adding Game Controller

---
+++ VenomGames/Controllers/GameController.cs
@@ -0,0 +1,103 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using VenomGames.Core.Contracts;
+using VenomGames.Core.DTOs.Game;
+using VenomGames.Infrastructure.Data.Models;
+
+namespace VenomGames.Controllers
+{
+    public class GameController : Controller
+    {
+        private readonly IGameService _gameService;
+
+        public GameController(IGameService gameService)
+        {
+            _gameService = gameService;
+        }
+
+        // GET: /Games
+        public async Task<IActionResult> Index()
+        {
+            var games = await _gameService.GetAllGamesAsync();
+            return View(games);
+        }
+
+        // GET: /Games/Details/5
+        public async Task<IActionResult> Details(int id)
+        {
+            var game = await _gameService.GetGameByIdAsync(id);
+            if (game == null)
+            {
+                return NotFound();
+            }
+            return View(game);
+        }
+
+        // GET: /Games/Create
+        public IActionResult Create()
+        {
+            return View();
+        }
+
+        // POST: /Games/Create
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Create(Game game)
+        {
+            if (ModelState.IsValid)
+            {
+                await _gameService.AddGameAsync(game);
+                return RedirectToAction(nameof(Index));
+            }
+            return View(game);
+        }
+
+        // GET: /Games/Edit/5
+        public async Task<IActionResult> Edit(int id)
+        {
+            var game = await _gameService.GetGameByIdAsync(id);
+            if (game == null)
+            {
+                return NotFound();
+            }
+            return View(game);
+        }
+
+        // POST: /Games/Edit/5
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Edit(int id, Game game)
+        {
+            if (id != game.GameId)
+            {
+                return NotFound();
+            }
+
+            if (ModelState.IsValid)
+            {
+                await _gameService.UpdateGameAsync(game);
+                return RedirectToAction(nameof(Index));
+            }
+            return View(game);
+        }
+
+        // GET: /Games/Delete/5
+        public async Task<IActionResult> Delete(int id)
+        {
+            var game = await _gameService.GetGameByIdAsync(id);
+            if (game == null)
+            {
+                return NotFound();
+            }
+            return View(game);
+        }
+
+        // POST: /Games/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> DeleteConfirmed(int id)
+        {
+            await _gameService.DeleteGameAsync(id);
+            return RedirectToAction(nameof(Index));
+        }
+    }
+}
