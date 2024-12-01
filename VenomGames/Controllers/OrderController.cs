9706b1523b20402a3337c3eaf875418f763e4179
PKoev99 <saov3v3v3@gmail.com>
2024-12-01 17:37:41 +0200

    Creating Order controller

---
+++ VenomGames/Controllers/OrderController.cs
@@ -0,0 +1,102 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using VenomGames.Core.Contracts;
+using VenomGames.Infrastructure.Data.Models;
+
+namespace VenomGames.Controllers
+{
+    public class OrderController : Controller
+    {
+        private readonly IOrderService _orderService;
+
+        public OrderController(IOrderService orderService)
+        {
+            _orderService = orderService;
+        }
+
+        // GET: /Orders
+        public async Task<IActionResult> Index()
+        {
+            var orders = await _orderService.GetAllOrdersAsync();
+            return View(orders);
+        }
+
+        // GET: /Orders/Details/5
+        public async Task<IActionResult> Details(int id)
+        {
+            var order = await _orderService.GetOrderByIdAsync(id);
+            if (order == null)
+            {
+                return NotFound();
+            }
+            return View(order);
+        }
+
+        // GET: /Orders/Create
+        public IActionResult Create()
+        {
+            return View();
+        }
+
+        // POST: /Orders/Create
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Create(Order order)
+        {
+            if (ModelState.IsValid)
+            {
+                await _orderService.CreateOrderAsync(order);
+                return RedirectToAction(nameof(Index));
+            }
+            return View(order);
+        }
+
+        // GET: /Orders/Edit/5
+        public async Task<IActionResult> Edit(int id)
+        {
+            var order = await _orderService.GetOrderByIdAsync(id);
+            if (order == null)
+            {
+                return NotFound();
+            }
+            return View(order);
+        }
+
+        // POST: /Orders/Edit/5
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Edit(int id, Order order)
+        {
+            if (id != order.OrderId)
+            {
+                return NotFound();
+            }
+
+            if (ModelState.IsValid)
+            {
+                await _orderService.UpdateOrderAsync(order);
+                return RedirectToAction(nameof(Index));
+            }
+            return View(order);
+        }
+
+        // GET: /Orders/Delete/5
+        public async Task<IActionResult> Delete(int id)
+        {
+            var order = await _orderService.GetOrderByIdAsync(id);
+            if (order == null)
+            {
+                return NotFound();
+            }
+            return View(order);
+        }
+
+        // POST: /Orders/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> DeleteConfirmed(int id)
+        {
+            await _orderService.DeleteOrderAsync(id);
+            return RedirectToAction(nameof(Index));
+        }
+    }
+}
