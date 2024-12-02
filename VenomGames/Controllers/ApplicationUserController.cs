008860b42b8a2249f156bfda03be4e9eb06d5775
PKoev99 <saov3v3v3@gmail.com>
2024-12-02 18:44:26 +0200

    Creating ApplicationUserController

---
+++ VenomGames/Controllers/ApplicationUserController.cs
@@ -0,0 +1,94 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using VenomGames.Core.Contracts;
+using VenomGames.Infrastructure.Data.Models;
+
+namespace VenomGames.Controllers
+{
+    public class ApplicationUserController : Controller
+    {
+        private readonly IApplicationUserService _userService;
+
+        public ApplicationUserController(IApplicationUserService userService)
+        {
+            _userService = userService;
+        }
+
+        // GET: /Users
+        public async Task<IActionResult> Index()
+        {
+            var users = await _userService.GetAllUsersAsync();
+            return View(users);
+        }
+
+        // GET: /Users/Details/5
+        public async Task<IActionResult> Details(string id)
+        {
+            var user = await _userService.GetUserByIdAsync(id);
+            if (user == null)
+            {
+                return NotFound();
+            }
+            return View(user);
+        }
+
+        // GET: /Users/Edit/5
+        public async Task<IActionResult> Edit(string id)
+        {
+            var user = await _userService.GetUserByIdAsync(id);
+            if (user == null)
+            {
+                return NotFound();
+            }
+            return View(user);
+        }
+
+        // POST: /Users/Edit/5
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Edit(string id, ApplicationUser user)
+        {
+            if (id != user.Id)
+            {
+                return NotFound();
+            }
+
+            if (ModelState.IsValid)
+            {
+                await _userService.UpdateUserAsync(user);
+                return RedirectToAction(nameof(Index));
+            }
+            return View(user);
+        }
+
+        // GET: /Users/Delete/5
+        public async Task<IActionResult> Delete(string id)
+        {
+            var user = await _userService.GetUserByIdAsync(id);
+            if (user == null)
+            {
+                return NotFound();
+            }
+            return View(user);
+        }
+
+        // POST: /Users/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> DeleteConfirmed(int id)
+        {
+            await _userService.DeleteUserAsync(id);
+            return RedirectToAction(nameof(Index));
+        }
+
+        // GET: /Users/ByEmail
+        public async Task<IActionResult> GetByEmail(string email)
+        {
+            var user = await _userService.GetUserByEmailAsync(email);
+            if (user == null)
+            {
+                return NotFound();
+            }
+            return View(user);
+        }
+    }
+}
