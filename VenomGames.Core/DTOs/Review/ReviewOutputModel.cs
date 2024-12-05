8ce01d42f3703756603384484228be5b8113c945
PKoev99 <saov3v3v3@gmail.com>
2024-12-05 22:48:05 +0200

    Small Warning message for possible null refference fixed.

--- VenomGames.Core/DTOs/Review/ReviewOutputModel.cs
+++ VenomGames.Core/DTOs/Review/ReviewOutputModel.cs
@@ -8,7 +8,7 @@
 
         public string UserId { get; set; } = string.Empty;
 
-        public string Content { get; set; } = string.Empty;
+        public string? Content { get; set; } = string.Empty;
 
         public decimal Rating { get; set; }
 
